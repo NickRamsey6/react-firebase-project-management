2307c97e574e728fb5e64e2324f14ff899d7bd67
Student <example@example.com>
2019-09-25 16:11:13 -0700

    finished the entire 40 part tutorial

---
+++ dist/precache-manifest.886447e697b16d96eaed7f913b2ef18c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd0fc52ee8dabaa5da162ed864872e10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ced76388c07dc908259",
+    "url": "/static/css/main.f2dbf98f.chunk.css"
+  },
+  {
+    "revision": "197ffe833135e8cc8e3e",
+    "url": "/static/js/2.993f4b11.chunk.js"
+  },
+  {
+    "revision": "2ced76388c07dc908259",
+    "url": "/static/js/main.1b1e6b58.chunk.js"
+  },
+  {
+    "revision": "352161fe39d7ebc281d8",
+    "url": "/static/js/runtime-main.5daf30de.js"
+  }
+]);
